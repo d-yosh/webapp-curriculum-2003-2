1ef9bb6bb2ac34a5e02f6035f03fda09e95c24d1
d-yosh <daicedaice87@gmail.com>
2020-01-15 21:22:45 +0900

    Main を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
-object Main {
-  val f1: Option[Int => Int] = Some((x) => x * 2)
-  val f2: Option[Int => Int] = Some((x) => x + 10)
-  val f3: Option[Int => Int] = Some((x) => x / 3)
+object Main extends App {
+  val f1: Option[Int => Int] = Some(x => x * 2)
+  val f2: Option[Int => Int] = Some(x => x + 10)
+  val f3: Option[Int => Int] = Some(x => x / 3)
+
+  val result = for {
+    f <- f1
+    g <- f2
+    h <- f3
+  } yield f.andThen(g).andThen(h)
+  println(result.map(_(15)))
 }
